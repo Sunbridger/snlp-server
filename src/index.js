8ddb2e543556289343077f56eb56f86add0eb003
七彩 <wangriqiao@cai-inc.com>
2021-06-07 19:55:44 +0800

    feat: 配置跨域

--- src/index.js
+++ src/index.js
@@ -14,6 +14,17 @@ const { saveAnswerData, saveQuestionData } = require('./mysql');
 const app = new Koa();
 let route = new Router();
 
+app.use(async (ctx, next)=> {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (ctx.method == 'OPTIONS') {
+    ctx.body = 200;
+  } else {
+    await next();
+  }
+});
+
 app.use(bodyParser());
 
 route.get('/search', async (ctx) => {
